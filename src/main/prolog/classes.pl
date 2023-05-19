3bbd419cd84f9d38a0bc16a34fedafecc25feff0
melburne <melburne42@gmail.com>
2023-05-19 18:06:34 -0500

    Add simple predicates for section 8.1

--- src/main/prolog/classes.pl
+++ src/main/prolog/classes.pl
@@ -1,34 +1,57 @@
+:- ensure_loaded(sectionlinks).
+
 %top_level_class.
 %nested_class.
-%member_class.
-%local_class.
-%anonymous_class.
+% member_class.
+% local_class.
+% anonymous_class.
+
+% instance_initializer.
+% static_initializer.
+% constructors.
+% fields.
+% methods.
+% classes.
+% interfaces.
 %inner_class.
 %enum_class.
 %record_class.
 
 
-nested_class :- member_class ; local_class ; anonymous_class.
+% section 8
+% nested_class :- member_class ; local_class ; anonymous_class.
 
-class_body :- members ; instance_initializer ; static_initializer ; constructors.
-members :- fields ; methods ; classes ; interfaces.
+% class_body :- members ; instance_initializer ; static_initializer ; constructors.
+% members :- fields ; methods ; classes ; interfaces.
 
-access_modifiers :- public ; protected ; private.
+access_modifiers(A):- 
+    A = ['public', 'protected', 'private'].
 
 overloaded(methods).
 overloaded(constructors).
 
-class_declaration :- normal_declaration ; enum_declaration ; record_declaration.
+% section 8.1
+class_declaration :- normal_declaration(Cm, Ti) ; enum_declaration ; record_declaration.
 
-class_modifier :- access_modifiers ; abstract ; static ; final ; sealed ; non_sealed ; strictfp.
+class_modifier(C):- 
+    access_modifiers(A),
+    append(A, ['abstract', 'static', 'final', 'sealed', 'non-sealed', 'strictfp'], C).
 
-class(inner_class) :- member_class =\= static ; local_class =\= static ; anonymous_class.
+% NormalClassDeclaration:
+%   {ClassModifier} class TypeIdentifier [TypeParameters]
+%   [ClassExtends] [ClassImplements] [ClassPermits] ClassBody
+normal_declaration(ClassModifier, TypeIdentifier) :- 
+    (class_modifier(C), member(ClassModifier, C)).
+    string(TypeIdentifier).
 
-class(static) :- member_enum_class ;
-                 local_enum_class ;
-                 member_record_class ;
-                 local_record_class ;
-                 member_class_interface.
+
+% class(inner_class) :- member_class =\= static ; local_class =\= static ; anonymous_class.
+
+% class(static) :- member_enum_class ;
+%                  local_enum_class ;
+%                  member_record_class ;
+%                  local_record_class ;
+%                  member_class_interface.
 
 
 
---
+++ src/main/prolog/sectionlinks.pl
@@ -0,0 +1,4 @@
+sectionlink(nested_class, "section 8").
+sectionlink(class_body, "section 8").
+sectionlink(members, "section 8").
+sectionlink(overloaded, "section 8").
