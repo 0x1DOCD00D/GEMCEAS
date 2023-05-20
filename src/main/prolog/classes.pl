03553d2fe2567b4ff8310305bb68c84f1762f90c
melburne <melburne42@gmail.com>
2023-05-20 12:13:30 -0500

    Complete section 8.1

--- src/main/prolog/classes.pl
+++ src/main/prolog/classes.pl
@@ -24,34 +24,38 @@
 % class_body :- members ; instance_initializer ; static_initializer ; constructors.
 % members :- fields ; methods ; classes ; interfaces.
 
-access_modifiers(A):- 
+access_modifiers(A) :- 
     A = ['public', 'protected', 'private'].
 
 overloaded(methods).
 overloaded(constructors).
 
 % section 8.1
-class_declaration :- normal_declaration(Cm, Ti) ; enum_declaration ; record_declaration.
+class_declaration :- 
+    normal_declaration(Cm, Ti, Ec, Ii, Pc, Cb);
+    enum_declaration;
+    record_declaration.
 
-class_modifier(C):- 
+class_modifier(C) :- 
     access_modifiers(A),
     append(A, ['abstract', 'static', 'final', 'sealed', 'non-sealed', 'strictfp'], C).
 
 % NormalClassDeclaration:
 %   {ClassModifier} class TypeIdentifier [TypeParameters]
 %   [ClassExtends] [ClassImplements] [ClassPermits] ClassBody
-normal_declaration(ClassModifier, TypeIdentifier) :- 
-    (class_modifier(C), member(ClassModifier, C)).
+normal_declaration(ClassModifier, TypeIdentifier, ExtendedClasses, ImplementedInterfaces, PermittedClasses, ClassBody) :- 
+    (class_modifier(C), member(ClassModifier, C)),
     string(TypeIdentifier).
-
-
-% class(inner_class) :- member_class =\= static ; local_class =\= static ; anonymous_class.
-
-% class(static) :- member_enum_class ;
-%                  local_enum_class ;
-%                  member_record_class ;
-%                  local_record_class ;
-%                  member_class_interface.
+    % TODO: implement checks for 'extends', 'implements', 'permits', 'classbody'
 
 
 
+/* 
+Sections to come back to:
+8.1.1.1 abstract Classes
+8.1.1.2 sealed, non-sealed, and final Classes
+8.1.2 Generic Classes and Type Parameters
+8.1.4 Superclasses and Subclasses
+8.1.5 Superinterfaces
+8.1.6 Permitted Direct Subclasses
+*/
