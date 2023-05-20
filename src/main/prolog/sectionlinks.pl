f15f10cbf18894c83cd86ffc27437651aac55171
melburne <melburne42@gmail.com>
2023-05-20 12:12:28 -0500

    Add section links

--- src/main/prolog/sectionlinks.pl
+++ src/main/prolog/sectionlinks.pl
@@ -2,3 +2,6 @@ sectionlink(nested_class, "section 8").
 sectionlink(class_body, "section 8").
 sectionlink(members, "section 8").
 sectionlink(overloaded, "section 8").
+sectionlink(class_declaration, "section 8.1").
+sectionlink(class_modifier, "section 8.1").
+sectionlink(normal_declaration, "section 8.1").
