6d65bdb378d71902bc7e5062ed9a9e7b521e9383
melburne <melburne42@gmail.com>
2023-05-20 19:57:54 -0500

    Add check for static classes

--- src/main/prolog/classes.pl
+++ src/main/prolog/classes.pl
@@ -1,29 +1,6 @@
 :- ensure_loaded(sectionlinks).
 
-%top_level_class.
-%nested_class.
-% member_class.
-% local_class.
-% anonymous_class.
-
-% instance_initializer.
-% static_initializer.
-% constructors.
-% fields.
-% methods.
-% classes.
-% interfaces.
-%inner_class.
-%enum_class.
-%record_class.
-
-
 % section 8
-% nested_class :- member_class ; local_class ; anonymous_class.
-
-% class_body :- members ; instance_initializer ; static_initializer ; constructors.
-% members :- fields ; methods ; classes ; interfaces.
-
 access_modifiers(A) :- 
     A = ['public', 'protected', 'private'].
 
@@ -32,7 +9,7 @@ overloaded(constructors).
 
 % section 8.1
 class_declaration :- 
-    normal_declaration(Cm, Ti, Ec, Ii, Pc, Cb);
+    normal_declaration(Cm, Ti, Ec, Ii, Pc, Cb, Ct);
     enum_declaration;
     record_declaration.
 
@@ -40,11 +17,18 @@ class_modifier(C) :-
     access_modifiers(A),
     append(A, ['abstract', 'static', 'final', 'sealed', 'non-sealed', 'strictfp'], C).
 
+% 'member' and 'inner' are both nested classes but inner classes cannot be static
+class_type(T) :-
+    T = ['top-level', 'member', 'inner'].
+
 % NormalClassDeclaration:
 %   {ClassModifier} class TypeIdentifier [TypeParameters]
 %   [ClassExtends] [ClassImplements] [ClassPermits] ClassBody
-normal_declaration(ClassModifier, TypeIdentifier, ExtendedClasses, ImplementedInterfaces, PermittedClasses, ClassBody) :- 
+normal_declaration(ClassModifier, TypeIdentifier, ExtendedClasses, ImplementedInterfaces, 
+    PermittedClasses, ClassBody, ClassType) :- 
     (class_modifier(C), member(ClassModifier, C)),
+    (class_type(T), member(ClassType, T)),
+    (ClassModifier == static -> ClassType == 'member'; true),
     string(TypeIdentifier).
     % TODO: implement checks for 'extends', 'implements', 'permits', 'classbody'
 
