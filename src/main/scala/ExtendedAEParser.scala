19efb3b098643d0152add0c1d213728050fe2b0c
Mark Grechanik <drmark@uic.edu>
2023-08-05 16:03:18 -0500

    added a raw parser for arith expressions, nothing fancy to test the generator

---
+++ src/main/scala/ExtendedAEParser.scala
@@ -0,0 +1,16 @@
+import ArithmeticExpressionParser.floatingPointNumber
+
+import scala.util.parsing.combinator.JavaTokenParsers
+
+
+object ExtendedAEParser extends JavaTokenParsers:
+  import scala.util.parsing.combinator.*
+
+  def expr: Parser[Any] = opt("+"|"-")~term~rep("+"~term | "-"~term)
+  def term: Parser[Any] = factor~rep("*"~factor | "/"~factor)
+  def factor: Parser[Any] = rep(("+"|"-")) ~ floatingPointNumber | "("~expr~")"
+
+  @main def runMain_ArithExpParser(): Unit =
+    parseAll(ExtendedAEParser.expr, "+(-+13   * (-2-1)/3.12)") match
+      case failed: ArithmeticExpressionParser.Failure => println(failed.msg)
+      case success => println(success)
