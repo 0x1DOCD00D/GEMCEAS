e3282e1d130a40c7c444ea4599c242a85399b896
Mark Grechanik <drmark@uic.edu>
2023-08-05 16:03:53 -0500

    removed parens

--- src/main/scala/ExtendedAEParser.scala
+++ src/main/scala/ExtendedAEParser.scala
@@ -8,7 +8,7 @@ object ExtendedAEParser extends JavaTokenParsers:
 
   def expr: Parser[Any] = opt("+"|"-")~term~rep("+"~term | "-"~term)
   def term: Parser[Any] = factor~rep("*"~factor | "/"~factor)
-  def factor: Parser[Any] = rep(("+"|"-")) ~ floatingPointNumber | "("~expr~")"
+  def factor: Parser[Any] = rep("+"|"-") ~ floatingPointNumber | "("~expr~")"
 
   @main def runMain_ArithExpParser(): Unit =
     parseAll(ExtendedAEParser.expr, "+(-+13   * (-2-1)/3.12)") match
